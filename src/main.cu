35e76285ff46d16acb4992a97920a94621cf9c27
herlin0793@gmail.com <herlin0793@gmail.com>
2022-04-23 12:31:54 -0400

    modiry render function

--- src/main.cu
+++ src/main.cu
@@ -48,7 +48,6 @@ void write_matrix_to_uint8(
 	const int w = R.rows();                              // Image width
 	const int h = R.cols();                              // Image height
 	const int comp = 4;                                  // 4 Channels Red, Green, Blue, Alpha
-	const int stride_in_bytes = w*comp;                  // Length of one row in bytes
 	image.resize(w*h*comp,0);         // The image itself;
 
 	for (unsigned wi = 0; wi < w; ++wi) {
@@ -100,7 +99,7 @@ __device__ vec3 color(const ray& r, hittable **world, curandState *local_rand_st
       return cur_attenuation * c;
     }
   }
-  return vec3(0.0,0.0,0.0); // exceeded recursion
+  return vec3(0.0,0.0,0.0); // exceeded recursion, return black
 }
 
 __global__ void render_init(int max_x, int max_y, curandState *rand_state) {
@@ -115,22 +114,25 @@ __global__ void render_init(int max_x, int max_y, curandState *rand_state) {
 __global__ void render(vec3 *fb, int max_x, int max_y, int ns, camera **cam, hittable **world, curandState *rand_state) {
   int i = threadIdx.x + blockIdx.x * blockDim.x;
   int j = threadIdx.y + blockIdx.y * blockDim.y;
-  if((i >= max_x) || (j >= max_y)) return;
-  int pixel_index = j*max_x + i;
-  curandState local_rand_state = rand_state[pixel_index];
-  vec3 col(0,0,0);
-  for(int s=0; s < ns; s++) {
-    float u = float(i + curand_uniform(&local_rand_state)) / float(max_x);
-    float v = float(j + curand_uniform(&local_rand_state)) / float(max_y);
-    ray r = (*cam)->get_ray(u, v, &local_rand_state);
-    col += color(r, world, &local_rand_state);
+  if (i < max_x and j < max_y) {
+    int pixel_index = j*max_x + i;
+    curandState local_rand_state = rand_state[pixel_index];
+    vec3 col(0,0,0);
+
+    // get pixel sample for ns times
+    for(int s=0; s < ns; s++) {
+      float u = float(i + curand_uniform(&local_rand_state)) / float(max_x);
+      float v = float(j + curand_uniform(&local_rand_state)) / float(max_y);
+      ray r = (*cam)->get_ray(u, v, &local_rand_state);
+      col += color(r, world, &local_rand_state);
+    }
+   
+    col /= float(ns);
+    col[0] = sqrt(col[0]);
+    col[1] = sqrt(col[1]);
+    col[2] = sqrt(col[2]);
+    fb[pixel_index] = col;
   }
-  rand_state[pixel_index] = local_rand_state;
-  col /= float(ns);
-  col[0] = sqrt(col[0]);
-  col[1] = sqrt(col[1]);
-  col[2] = sqrt(col[2]);
-  fb[pixel_index] = col;
 }
 
 __global__ void rand_init(curandState *rand_state) {
@@ -238,7 +240,7 @@ int main (int argc, char** argv) {
   int nx, ny;
   int tx = 8;
   int ty = 8;
-  int ns = 100;
+  int ns = 100; // sample per pixel
 
   if (PROFILE_RENDER) {
     nx = 400;
